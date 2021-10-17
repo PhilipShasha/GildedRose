a6033cafb872588e7c4f53479986d03dff233d86
Philip <59822183+PhilipShasha@users.noreply.github.com>
2021-10-17 23:21:19 +1300

    integrated quality checks into a method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -21,25 +21,17 @@ class Shop {
 
   updateItemQuality(item) {
     if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (item.quality > 0) {
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.quality = item.quality - 1;
-        }
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        this.adjustQuality(item, -1);
       }
     } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
+      this.adjustQuality(item, 1);
+      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.sellIn < 11) {
+          this.adjustQuality(item, 1);
+        }
+        if (item.sellIn < 6) {
+          this.adjustQuality(item, 1);
         }
       }
     }
@@ -49,21 +41,25 @@ class Shop {
     if (item.sellIn < 0) {
       if (item.name != 'Aged Brie') {
         if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-              item.quality = item.quality - 1;
-            }
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            this.adjustQuality(item, -1);
           }
         } else {
-          item.quality = item.quality - item.quality;
+          this.adjustQuality(item, -item.quality);
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
+        this.adjustQuality(item, 1);
       }
     }
   }
+
+  adjustQuality(item, adjustment) {
+    let newQuality = item.quality + adjustment;
+    if (newQuality <= 50 && newQuality >= 0){
+      item.quality = newQuality
+    }
+  }
+
 }
 module.exports = {
   Item,
